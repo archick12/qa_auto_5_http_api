d1aec35e32f53ab30615c2109674111a2090dbcf
archick12 <a.a.piluck@gmail.com>
2018-02-19 20:55:10 +0200

    refactoring

---
+++ src/main/java/utils/api/JiraApiActions.java
@@ -0,0 +1,80 @@
+package utils.api;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.testng.Assert;
+import utils.data.JiraJsonObjectHelper;
+
+/**
+ * Wrapper class for all possible actions in JIRA API
+ */
+public class JiraApiActions {
+
+  public static ValidatableResponse createIssue(String issueJSON) {
+    ValidatableResponse response = HTTPMethods.post(APIPathes.issue, issueJSON);
+    Assert.assertEquals(response.extract().statusCode(), 201);
+    Assert.assertTrue(response.extract().contentType().contains(ContentType.JSON.toString()));
+    return response.extract().path("key");
+  }
+
+  public static ValidatableResponse getIssue(String issueKey) {
+    ValidatableResponse response = HTTPMethods.get(APIPathes.issue + issueKey);
+    Assert.assertEquals(response.extract().statusCode(), 200);
+    Assert.assertTrue(response.extract().contentType().contains(ContentType.JSON.toString()));
+    return response;
+  }
+
+  public static ValidatableResponse getNonExistingIssue(String issueKey) {
+    ValidatableResponse response = HTTPMethods.get(APIPathes.issue + issueKey);
+    Assert.assertEquals(response.extract().statusCode(), 404);
+    Assert.assertTrue(response.extract().contentType().contains(ContentType.JSON.toString()));
+    return response;
+  }
+
+  public static void deleteIssue(String issueKey) {
+    ValidatableResponse response = HTTPMethods.delete(APIPathes.issue + issueKey);
+    Assert.assertEquals(response.extract().statusCode(), 204);
+    Assert.assertTrue(response.extract().contentType().contains(ContentType.JSON.toString()));
+  }
+
+  public static ValidatableResponse createComment(String issueId, String comment) {
+    String jsonForAddComment = JiraJsonObjectHelper.generateJSONForComment(comment);
+    ValidatableResponse response = HTTPMethods
+        .post(String.format(APIPathes.commentInIssue, issueId), jsonForAddComment);
+
+    // response.log().all();
+    response.statusCode(200);
+    response.contentType(ContentType.JSON);
+    return response;
+  }
+
+  public static ValidatableResponse updateComment(String issueId, String commentId,
+      String comment) {
+    String jsonForComment = JiraJsonObjectHelper.generateJSONForComment(comment);
+    ValidatableResponse response = HTTPMethods
+        .put(String.format(APIPathes.existingCommentInIssue, issueId, commentId), jsonForComment);
+
+    response.statusCode(200);
+    response.contentType(ContentType.JSON);
+    return response;
+  }
+
+  public static ValidatableResponse deleteComment(String issueId, String commentId) {
+    ValidatableResponse response = HTTPMethods
+        .delete(String.format(APIPathes.existingCommentInIssue, issueId, commentId));
+
+    response.statusCode(204);
+    response.contentType(ContentType.JSON);
+    return response;
+  }
+
+  public static void getNonExistingComment(String issueId, String commentId) {
+    ValidatableResponse response = HTTPMethods
+        .get(String.format(APIPathes.existingCommentInIssue, issueId, commentId));
+    response.statusCode(404);
+    response.contentType(ContentType.JSON);
+  }
+}
--- src/test/java/RestAPIIssueTests.java
+++ src/test/java/RestAPIIssueTests.java
@@ -1,169 +1,116 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import utils.api.Authorization;
+import utils.api.JiraApiActions;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 import static org.testng.Assert.assertEquals;
 
 public class RestAPIIssueTests {
-    String sessionId = "";
-    String projectId = "10502";
-    String issueType = "13561";
 
-    @Test(groups = {"CRITICAL", "HTTP"})
-    public void authentication(){
-        RestAssured.baseURI = "http://jira.hillel.it:8080";
-        String credentialsJSON = "{" +
-                "\"username\": \"qa_auto_5_team_1\"," +
-                "\"password\": \"qa_auto_5_team_1\"" +
-                "} "
-                ;
+  String issueId = "13561";
 
-        /* HTTP Request for login*/
-        sessionId = given().
-            header("Content-Type", "application/json").
-            body(credentialsJSON).
-            when().
-            post("/rest/auth/1/session").
-            then().
-            extract().
-            path("session.value");
+  @Test(groups = {"CRITICAL", "HTTP"})
+  public void authentication() {
+    Authorization.loginToJIRA();
+  }
 
-        System.out.printf("\nSESSION: "+ sessionId);
-}
+  @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
+  public void commentCRUD() {
+    /* HTTP Request for Create Comment*/
+    String comment = "test";
+    ValidatableResponse response = JiraApiActions.createComment(issueId, comment);
+    String myCommentFromServer = response.extract().path("body");
+    String commentId = response.extract().path("id").toString();
+    assertEquals(comment, myCommentFromServer);
 
-    @Test(groups = {"Regression", "HTTP"},dependsOnGroups = {"CRITICAL"})
-    public void commentCRUD(){
-        RestAssured.baseURI = "http://jira.hillel.it:8080";
-        ValidatableResponse response;
-        String myComment= "test";
-        String jsonForAddComment = "{\"body\" : \"" + myComment + "\"}";
-
-        /* HTTP Request for addComment*/
-        response = given().
-                header("Content-Type", "application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                body(jsonForAddComment).
-                when().
-                post("/rest/api/2/issue/13561/comment").
-                then();
-
-        String myCommentFromServer =response.extract().path("body");
-        String wholeJSON =response.extract().asString();
-        String commentId = response.extract().path("id").toString();
-        response.log().all();
-        response.statusCode(201);
-        response.contentType(ContentType.JSON);
-        assertEquals(myComment,myCommentFromServer );
-
-         /* HTTP Request for Update Comment*/
-        String newComment = "New Comment";
-        String jsonForUpdateComment = "{\"body\" : \"" + newComment + "\"}";
-        response = given().
-                header("Content-Type", "application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                body(jsonForUpdateComment).
-                when().
-                put("/rest/api/2/issue/13561/comment/" + commentId).
-                then().log().all().
-                statusCode(200);
-        String newCommentFromServer =response.extract().path("body");
-        assertEquals(newComment,newCommentFromServer);
+    /* HTTP Request for Update Comment*/
+    String newComment = "New Comment";
+    response = JiraApiActions.updateComment(issueId, commentId, newComment);
+    String newCommentFromServer = response.extract().path("body");
+    assertEquals(newComment, newCommentFromServer);
         
-        /* HTTP Request for Delete Comment*/
-        given().
-                header("Content-Type", "application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                when().
-                delete("/rest/api/2/issue/13561/comment/" + commentId).
-                then().log().all().
-                statusCode(204);
-        String responseBody = response.extract().asString();
+    /* HTTP Request for Delete Comment*/
+    JiraApiActions.deleteComment(issueId, commentId);
+    String responseBody = response.extract().asString();
 
-        /* HTTP Request for confirm that comment was deleted*/
-        response = given().
-                header("Content-Type", "application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                when().
-                get("/rest/api/2/issue/13561/comment/" + commentId).
-                then().log().all().
-                statusCode(404).contentType(ContentType.JSON);
+    /* HTTP Request for confirm that comment was deleted*/
+    JiraApiActions.getNonExistingComment(issueId, commentId);
+  }
 
-        System.out.printf("\nRESPONSE: " + responseBody);
-    }
-    @Test(groups = {"Regression", "HTTP"},dependsOnGroups = {"CRITICAL"})
-    public void descriptionCRUD(){
-        RestAssured.baseURI = "http://jira.hillel.it:8080";
-        ValidatableResponse response;
-        String description = "My description";
-        String jsonForAddDescription = "{\"fields\":{" + "\"description\": \"My description\""+"}}";
+  @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
+  public void descriptionCRUD() {
+    RestAssured.baseURI = "http://jira.hillel.it:8080";
+    ValidatableResponse response;
+    String description = "My description";
+    String jsonForAddDescription = "{\"fields\":{" + "\"description\": \"My description\"" + "}}";
 
           /* HTTP Request for add description to issue*/
-        response = given().
-                header("Content-Type", "application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                body(jsonForAddDescription).
-                when().
-                put("/rest/api/2/issue/13561").
-                then().log().all();
+    response = given().
+        header("Content-Type", "application/json").
+        header("Cookie", "JSESSIONID=" + Authorization.JSESSIONID).
+        body(jsonForAddDescription).
+        when().
+        put("/rest/api/2/issue/13561").
+        then().log().all();
 
-        String responseBody = response.extract().asString();
-        String jsonForDeleteDescription = "{\"fields\":{" + "\"description\": \"\""+"}}";
+    String responseBody = response.extract().asString();
+    String jsonForDeleteDescription = "{\"fields\":{" + "\"description\": \"\"" + "}}";
 
         /* HTTP Request for delete description from issue*/
-        response = given().
-                header("Content-Type", "application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                body(jsonForDeleteDescription).
-                when().
-                put("/rest/api/2/issue/13561").
-                then().log().all().
-                statusCode(204).contentType(ContentType.JSON);
+    response = given().
+        header("Content-Type", "application/json").
+        header("Cookie", "JSESSIONID=" + Authorization.JSESSIONID).
+        body(jsonForDeleteDescription).
+        when().
+        put("/rest/api/2/issue/13561").
+        then().log().all().
+        statusCode(204).contentType(ContentType.JSON);
 
-    }
-    @Test(groups = {"Regression", "HTTP"},dependsOnGroups = {"CRITICAL"})
-    public void remoteIssueLinksCRUD() {
-        RestAssured.baseURI = "http://jira.hillel.it:8080";
-        ValidatableResponse response;
-        String jsonForAddRemoteLink = "{\"object\": {\n" +
-                "\"url\": \"https://obmenka.od.ua/\",\n" +
-                "\"title\": \"obmenka\"\n" +
-                "}}";
-        String result = given().
-                header("Accept","application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                when().
-                get("/rest/api/2/issue/13561/remotelink").
-                then().
-                log().all().
-                extract().
-                asString();
+  }
+
+  @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
+  public void remoteIssueLinksCRUD() {
+    RestAssured.baseURI = "http://jira.hillel.it:8080";
+    ValidatableResponse response;
+    String jsonForAddRemoteLink = "{\"object\": {\n" +
+        "\"url\": \"https://obmenka.od.ua/\",\n" +
+        "\"title\": \"obmenka\"\n" +
+        "}}";
+    String result = given().
+        header("Accept", "application/json").
+        header("Cookie", "JSESSIONID=" + Authorization.JSESSIONID).
+        when().
+        get("/rest/api/2/issue/13561/remotelink").
+        then().
+        log().all().
+        extract().
+        asString();
 
         /* create new link to Remote Issue */
-        response = given().
-                header("Accept","application/json").
-                header("Content-Type","application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                body(jsonForAddRemoteLink).
-                when().
-                post("/rest/api/2/issue/13561/remotelink").
-                then().log().all().
-                statusCode(201).contentType(ContentType.JSON);
-        String responseBody = response.extract().asString();
-        String linkId = response.extract().path("id").toString();
+    response = given().
+        header("Accept", "application/json").
+        header("Content-Type", "application/json").
+        header("Cookie", "JSESSIONID=" + Authorization.JSESSIONID).
+        body(jsonForAddRemoteLink).
+        when().
+        post("/rest/api/2/issue/13561/remotelink").
+        then().log().all().
+        statusCode(201).contentType(ContentType.JSON);
+    String responseBody = response.extract().asString();
+    String linkId = response.extract().path("id").toString();
 
         /* delete link to Remote Issue */
-        given().
-                header("Content-Type", "application/json").
-                header("Cookie", "JSESSIONID=" + sessionId).
-                body(jsonForAddRemoteLink).
-                when().
-                delete("/rest/api/2/issue/" + issueType +"/remotelink/"+ linkId).
-                then().log().all().
-                statusCode(204).contentType(ContentType.JSON);
+    given().
+        header("Content-Type", "application/json").
+        header("Cookie", "JSESSIONID=" + Authorization.JSESSIONID).
+        body(jsonForAddRemoteLink).
+        when().
+        delete("/rest/api/2/issue/" + issueId + "/remotelink/" + linkId).
+        then().log().all().
+        statusCode(204).contentType(ContentType.JSON);
 
-    }
+  }
 }
