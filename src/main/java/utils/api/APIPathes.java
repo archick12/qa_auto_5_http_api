bddccf960307bbde9a8790108cd5d1f5e425f10b
JuliaPokitko <pokitkojulia@gmail.com>
2018-02-20 12:46:33 +0200

    create path variables

--- src/main/java/utils/api/APIPathes.java
+++ src/main/java/utils/api/APIPathes.java
@@ -6,5 +6,8 @@ public interface APIPathes {
   String issue = "/rest/api/2/issue/";
   String commentInIssue = "/rest/api/2/issue/%s/comment";
   String existingCommentInIssue = "/rest/api/2/issue/%s/comment/%s";
+  String descriptionInIssue = "/rest/api/2/issue/%s";
+  String remoteIssueLink = "/rest/api/2/issue/%s/remotelink";
+  String existingRemoteIssueLink = "/rest/api/2/issue/%s/remotelink/\" + linkId";
 
 }
