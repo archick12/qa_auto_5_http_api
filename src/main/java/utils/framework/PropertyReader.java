cb9b3d31bf37c81cb44bccd7a847431f2a97077f
marinamv <marina093746075@gmail.com>
2018-02-26 12:04:19 +0200

    Added PropertyReader

---
+++ src/main/java/utils/framework/PropertyReader.java
@@ -0,0 +1,62 @@
+package utils.framework;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class PropertyReader {
+
+    // TUTORIAL - http://www.avajava.com/tutorials/lessons/how-do-i-read-a-properties-file.html
+    public static Map<String, String> readProperties(String propertyFile) {
+        Properties propertyFileValues = new Properties();
+        FileInputStream input = null;
+        HashMap<String, String> result;
+
+        // TODO ------ 1) opening the file for write access -----------
+        try {
+            try {
+                input = new FileInputStream(propertyFile);
+                try {
+                    propertyFileValues.load(input);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // TODO ------ 1) end
+
+            // TODO ------ 2) read actual values from the file -----------
+            result = new HashMap();
+            result.put("username", propertyFileValues.getProperty("username"));
+            result.put("password", propertyFileValues.getProperty("password"));
+            // TODO ------ 2) end
+
+        } finally {
+            // TODO ------ 3) closing connection/stream to the file
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            // TODO ------ 3) end
+        }
+
+        // TODO ------ 4) returning result
+        return result;
+        // TODO ------ 4) end
+    }
+
+
+    public static void main(String[] args) {
+        Map<String, String> collectionOfKeyValueParameters = readProperties("jira.properties");
+        String userName = collectionOfKeyValueParameters.get("username");
+        System.out.println("USER NAME: " + userName);
+
+    }
+}
