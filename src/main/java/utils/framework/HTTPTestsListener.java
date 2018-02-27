c6f9185ae5e83cb6cf1f5bfb8723e93eae01b657
Dima Spektor <d.spektor.v@gmail.com>
2018-02-27 18:20:57 +0200

    [+] @TestCase annotation. [+] getTestCaseId() to Listener. [+] @TestCase to searchTests Added. [+] assert to CRITICAL group

--- src/main/java/utils/framework/HTTPTestsListener.java
+++ src/main/java/utils/framework/HTTPTestsListener.java
@@ -1,43 +1,65 @@
 package utils.framework;
 
+import org.apache.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import utils.api.Authorization;
+
+import java.lang.reflect.Method;
 import java.util.Map;
+
+import utils.TestCase;
 
 public class HTTPTestsListener implements ITestListener {
 
-  final static PropertyReader propertyReader = new PropertyReader();
-  public static Map<String, String> properties = propertyReader
-          .readProperties("jira.properties");
+    final static PropertyReader propertyReader = new PropertyReader();
+    public static Map<String, String> properties = propertyReader
+            .readProperties("jira.properties");
+    static final Logger logger = Logger.getLogger(HTTPTestsListener.class);
 
 
-  public void onTestStart(ITestResult iTestResult) {
-      // TODO
-  }
+    public void onTestStart(ITestResult iTestResult) {
+        // TODO
+    }
 
-  public void onTestSuccess(ITestResult iTestResult) {
+    public void onTestSuccess(ITestResult iTestResult) {
+        String testCaseId = getTestCaseId(iTestResult);
+        logger.info("Test Case id: " + testCaseId);
+    }
 
-  }
+    public void onTestFailure(ITestResult iTestResult) {
 
-  public void onTestFailure(ITestResult iTestResult) {
+    }
 
-  }
+    public void onTestSkipped(ITestResult iTestResult) {
 
-  public void onTestSkipped(ITestResult iTestResult) {
+    }
 
-  }
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
 
-  public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+    }
 
-  }
+    public void onStart(ITestContext iTestContext) {
+        Authorization.loginToJIRA();
+    }
 
-  public void onStart(ITestContext iTestContext) {
-    Authorization.loginToJIRA();
-  }
+    public void onFinish(ITestContext iTestContext) {
 
-  public void onFinish(ITestContext iTestContext) {
+    }
 
-  }
+    public String getTestCaseId(ITestResult iTestResult) {
+        Class myClass = iTestResult.getTestClass().getRealClass();
+        Method method = null;
+        try {
+            String methodName = iTestResult.getMethod().getMethodName();
+            method = myClass.getMethod(methodName);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        TestCase testCaseAnnotation = method.getAnnotation(TestCase.class);
+        logger.debug("ANNOTATION: " + testCaseAnnotation);
+        return testCaseAnnotation.id();
+    }
 }
--- src/test/java/RestAPISearchTests.java
+++ src/test/java/RestAPISearchTests.java
@@ -1,77 +1,81 @@
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.apache.log4j.Logger;
+import utils.TestCase;
 import utils.api.Authorization;
 import utils.api.JiraApiActions;
 
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class RestAPISearchTests {
 
-  static final Logger logger = Logger.getLogger(RestAPISearchTests.class);
+    static final Logger logger = Logger.getLogger(RestAPISearchTests.class);
 
 
-  @Test(groups = {"CRITICAL", "HTTP"})
-  public void authentication() {
-    Authorization.loginToJIRA();
-  }
-
-  @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
-  public void searchByProject() {
-    /* HTTP Request for search for issues by project*/
-    String projectName = "QAAuto5";
-    ValidatableResponse response = JiraApiActions.searchForIssues("project = " + projectName);
-    List<String> searchResultIssuesProjectName = response.extract().jsonPath()
-        .getList("issues.fields.project.name");
-    for (String item : searchResultIssuesProjectName) {
-      assertEquals(true, item.contains(projectName));
-      logger.info("Assertion passed: " + item);
+    @Test(groups = {"CRITICAL", "HTTP"})
+    public void authentication() {
+        assertNotNull(Authorization.JSESSIONID);
     }
-  }
 
-  @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
-  public void searchByCurrentUserAsAssignee() {
-    /* HTTP Request for search for issues by Assignee - Current User*/
-    String assignee = Authorization.username;
-    ValidatableResponse response = JiraApiActions.searchForIssues("assignee = " + assignee);
-    List<String> searchResultIssuesAssignee = response.extract().jsonPath()
-        .getList("issues.fields.assignee");
-    for (String item : searchResultIssuesAssignee) {
-      assertEquals(true, item.contains(assignee));
-      logger.info("Assertion passed: " + item);
+    @TestCase(id = "T1")
+    @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
+    public void searchByProject() {
+        /* HTTP Request for search for issues by project*/
+        String projectName = "QAAuto5";
+        ValidatableResponse response = JiraApiActions.searchForIssues("project = " + projectName);
+        List<String> searchResultIssuesProjectName = response.extract().jsonPath()
+                .getList("issues.fields.project.name");
+        for (String item : searchResultIssuesProjectName) {
+            assertEquals(true, item.contains(projectName));
+            logger.info("Assertion passed: " + item);
+        }
     }
-  }
 
-  @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
-  public void searchByUnassignedAsAssignee() {
-    /* HTTP Request for search for issues by Assignee - Unassigned*/
-    String assignee = "Unassigned";
-    ValidatableResponse response = JiraApiActions.searchForIssues("assignee = " + assignee);
-    List<String> searchResultIssuesAssignee = response.extract().jsonPath()
-        .getList("issues.fields.assignee");
-    for (String item : searchResultIssuesAssignee) {
-      assertEquals(true, item.contains(assignee));
-      logger.info("Assertion passed: " + item);
+    @TestCase(id = "T2")
+    @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
+    public void searchByCurrentUserAsAssignee() {
+        /* HTTP Request for search for issues by Assignee - Current User*/
+        String assignee = Authorization.username;
+        ValidatableResponse response = JiraApiActions.searchForIssues("assignee = " + assignee);
+        List<String> searchResultIssuesAssignee = response.extract().jsonPath()
+                .getList("issues.fields.assignee");
+        for (String item : searchResultIssuesAssignee) {
+            assertEquals(true, item.contains(assignee));
+            logger.info("Assertion passed: " + item);
+        }
     }
-  }
+
+    @TestCase(id = "T3")
+    @Test(groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
+    public void searchByUnassignedAsAssignee() {
+        /* HTTP Request for search for issues by Assignee - Unassigned*/
+        String assignee = "Unassigned";
+        ValidatableResponse response = JiraApiActions.searchForIssues("assignee = " + assignee);
+        List<String> searchResultIssuesAssignee = response.extract().jsonPath()
+                .getList("issues.fields.assignee");
+        for (String item : searchResultIssuesAssignee) {
+            assertEquals(true, item.contains(assignee));
+            logger.info("Assertion passed: " + item);
+        }
+    }
 
     @DataProvider
-    public Object[][] getIssueTypesData(){
-        return new Object[][]{{"Bug"},{"Story"},{"Epic"},{"Improvement"},{"Task"},{"Sub-task"},{"Sub-Defect"}};
+    public Object[][] getIssueTypesData() {
+        return new Object[][]{{"Bug"}, {"Story"}, {"Epic"}, {"Improvement"}, {"Task"}, {"Sub-task"}, {"Sub-Defect"}};
     }
 
-    @Test(dataProvider = "getIssueTypesData",groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
+    @TestCase(id = "T4")
+    @Test(dataProvider = "getIssueTypesData", groups = {"Regression", "HTTP"}, dependsOnGroups = {"CRITICAL"})
     public void searchIssuesByDifferentType(String type) {
-      ValidatableResponse response = JiraApiActions.searchForIssues("issuetype = " + type);
-      List<String> searchResult = response.extract().jsonPath()
-              .getList("issues.fields.issuetype.name");
-      for (String item : searchResult) {
-          assertEquals(true, item.contains(type));
-      }
+        ValidatableResponse response = JiraApiActions.searchForIssues("issuetype = " + type);
+        List<String> searchResult = response.extract().jsonPath()
+                .getList("issues.fields.issuetype.name");
+        for (String item : searchResult) {
+            assertEquals(true, item.contains(type));
+        }
     }
 }
