6ede87fb05808eecdbf2dabc4da02dce31156130
Dima Spektor <d.spektor.v@gmail.com>
2018-02-27 18:15:23 +0200

    Added logger to Authorization, removed log().all()

--- src/main/java/utils/api/Authorization.java
+++ src/main/java/utils/api/Authorization.java
@@ -1,7 +1,7 @@
 package utils.api;
 
 import io.restassured.RestAssured;
-// import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
 import io.restassured.http.ContentType;
 import static io.restassured.path.json.JsonPath.given;
 import utils.data.JiraJsonObjectHelper;
@@ -11,28 +11,28 @@ import static io.restassured.RestAssured.given;
 
 public class Authorization {
 
-  public static String JSESSIONID;
-  public static String BASE_URI = "http://jira.hillel.it:8080";
-  public static String username = HTTPTestsListener.properties.get("username");
-  public static String  password = HTTPTestsListener.properties.get("password");
-//  static final Logger logger = Logger.getLogger(Authorization.class);
+    public static String JSESSIONID;
+    public static String BASE_URI = "http://jira.hillel.it:8080";
+    public static String username = HTTPTestsListener.properties.get("username");
+    public static String password = HTTPTestsListener.properties.get("password");
+    static final Logger logger = Logger.getLogger(Authorization.class);
 
-  public static void loginToJIRA() {
-    RestAssured.baseURI = BASE_URI;
+    public static void loginToJIRA() {
+        RestAssured.baseURI = BASE_URI;
 
-    String loginJson = JiraJsonObjectHelper.generateJSONForLogin();
-    JSESSIONID =
-        given().
-            header("Content-Type", ContentType.JSON).
-            body(loginJson).
-            when().
-            post(APIPathes.login).
-            then().
-            statusCode(200).contentType(ContentType.JSON).
-            log().all().
-            extract().
-            path("session.value");
+        String loginJson = JiraJsonObjectHelper.generateJSONForLogin();
+        JSESSIONID =
+                given().
+                        header("Content-Type", ContentType.JSON).
+                        body(loginJson).
+                        when().
+                        post(APIPathes.login).
+                        then().
+                        statusCode(200).contentType(ContentType.JSON).
+//                        log().all().
+                        extract().
+                        path("session.value");
 
-    // logger.info("\nAUTHORIZATION TOKEN: " + Authorization.JSESSIONID);
-  }
+        logger.info("\nAUTHORIZATION TOKEN: " + Authorization.JSESSIONID);
+    }
 }
