b9dbc4f88907c4f638e957b501aadc5e69092377
adikalova <adikalova@gmail.com>
2018-02-27 16:56:49 +0200

    add JiraIdAnnotannotation to ListenerClass

--- src/main/java/utils/framework/HTTPTestsListener.java
+++ src/main/java/utils/framework/HTTPTestsListener.java
@@ -4,6 +4,9 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import utils.api.Authorization;
+import utils.api.JiraIdAnnotation;
+
+import java.lang.reflect.Method;
 import java.util.Map;
 
 public class HTTPTestsListener implements ITestListener {
@@ -13,8 +16,8 @@ public class HTTPTestsListener implements ITestListener {
           .readProperties("jira.properties");
 
 
-  public void onTestStart(ITestResult iTestResult) {
-      // TODO
+  public void onTestStart(ITestResult result) {
+      getJiraId(result);
   }
 
   public void onTestSuccess(ITestResult iTestResult) {
@@ -39,5 +42,22 @@ public class HTTPTestsListener implements ITestListener {
 
   public void onFinish(ITestContext iTestContext) {
 
+  }
+  public void getJiraId (ITestResult result){
+
+    Class myClass = result.getTestClass().getRealClass();
+    Method method = null; //
+    try {
+      String methodName = result.getMethod().getMethodName();
+      method = myClass.getMethod(methodName); // спрашиваем Java: "Как называется, метод внутри которого
+      // ты сейчас выполняешь этот кусочек кода". Проще говоря - "Где я выполнился?"
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    }
+
+    JiraIdAnnotation testJiraIdAnnotation = method.getAnnotation(JiraIdAnnotation.class); // Где бы я не выполнялся, Java верни
+    // аннотацию из метода в котором я выполняюсь. Похожим образом можно сделать для класса.
+    System.out.println("ANNOTATION: " + testJiraIdAnnotation);
+    System.out.println("Jira id: " + testJiraIdAnnotation.id()); // верни значение внутри аннотации
   }
 }
